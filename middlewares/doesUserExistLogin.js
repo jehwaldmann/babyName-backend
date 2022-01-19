c85b4434fc0c4703a7014f9c7bb99125df86a5d6
jehwaldmann <90551022+jehwaldmann@users.noreply.github.com>
2022-01-19 14:35:41 +0200

    (WIP) user route

---
+++ middlewares/doesUserExistLogin.js
@@ -0,0 +1,14 @@
+const { getUserByEmail } = require("../query/userQueries");
+
+async function doesUserExistLogin(req, res, next) {
+  const { email } = req.body;
+  const user = await getUserByEmail(email);
+  if (!user) {
+    res.status(400).send("User with this email does not exist");
+    return;
+  }
+  req.body.user = user;
+  next();
+}
+
+module.exports = { doesUserExistLogin };
